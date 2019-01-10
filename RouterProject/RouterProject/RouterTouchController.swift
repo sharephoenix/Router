b2d739141c5069100324314baa03efdd574ebde2
alexluan <326083325@qq.com>
2019-01-10 22:26:38 +0800

    Main

---
+++ RouterProject/RouterProject/RouterTouchController.swift
@@ -0,0 +1,18 @@
+//
+//  RouterTouchController.swift
+//  RouterProject
+//
+//  Created by Alexluan on 2019/1/10.
+//  Copyright © 2019年 Alexluan. All rights reserved.
+//
+
+import UIKit
+
+class RouterTouchController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
